265a1b85527c5f55a0cba2e8286eb517693c4254
Larry Garfield <larry@garfieldtech.com>
2019-03-16 15:41:39 -0400

    Add more class mappings to prepare for refactoring.

--- tests/Parser/ParserTest.php
+++ tests/Parser/ParserTest.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 namespace Crell\Xavier\Parser;
 
 use Crell\Xavier\Elements\XmlElement;
+use Crell\Xavier\Parser\Elements\billTo;
+use Crell\Xavier\Parser\Elements\comment;
 use Crell\Xavier\Parser\Elements\purchaseOrder;
+use Crell\Xavier\Parser\Elements\shipTo;
 use PHPUnit\Framework\TestCase;
 
-class MockParser extends Parser {
+class MockParser extends Parser
+{
     protected function mapTagToClass(string $tag): string
     {
         $map = [
             'purchaseOrder' => purchaseOrder::class,
+            'shipTo' => shipTo::class,
+            'billTo' => billTo::class,
+            'comment' => comment::class,
         ];
 
         return $map[$tag] ?? parent::mapTagToClass($tag);
@@ -27,10 +34,19 @@ class ParserTest extends TestCase
         $p = new MockParser();
         $result = $p->parseFile($filename);
 
-        print_r($result);
+        //print_r($result);
 
         $this->assertInstanceOf(purchaseOrder::class, $result);
         $this->assertEquals('1999-10-20', $result['orderDate']);
         $this->assertEquals('', (string)$result);
+
+        $this->assertInstanceOf(shipTo::class, $result->children[0]);
+        $this->assertInstanceOf(billTo::class, $result->children[1]);
+        $this->assertInstanceOf(comment::class, $result->children[2]);
+        $this->assertEquals('Hurry, my lawn is going wild', $result->children[2]);
+
+        $this->assertInstanceOf(XmlElement::class, $result->children[0]->children[0]);
+        $this->assertEquals('Alice Smith', $result->children[0]->children[0]);
+
     }
 }
