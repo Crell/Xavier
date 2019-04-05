38f036da292a0d00929cf3d22fe0b2403f40c8ff
Larry Garfield <larry@garfieldtech.com>
2019-04-05 13:12:52 -0500

    Add missing exceptions.

---
+++ src/NoNamespaceMapDefined.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Crell\Xavier;
+
+class NoNamespaceMapDefined extends \RuntimeException
+{
+    public static function create(string $namespace) : self
+    {
+        $message = sprintf('The XML namespace %s has no corresponding PHP namespace defined.', $namespace);
+        return new static($message);
+    }
+}
---
+++ src/UnknownNamespaceInFile.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Crell\Xavier;
+
+class UnknownNamespaceInFile extends \RuntimeException
+{
+    public static function create(string $tagNs) : self
+    {
+        $message = sprintf('The short namespace %s is not defined in the root element.', $tagNs);
+        return new static($message);
+    }
+}
